1f7d8a6f27d11c3b56bbb394b8633158814f421d
Rahul Prajapati <rahulp@ifourtechnolab.com>
2023-01-17 16:02:35 +0530

    without jsx implement.

--- hello-world/src/components/Hello.js
+++ hello-world/src/components/Hello.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Hello = () => {
-    return (
-        <div className="dummyClass">
-            <h1>Hello Rahul</h1>
-        </div>
+    // return (
+    //     <div className="dummyClass">
+    //         <h1>Hello Rahul</h1>
+    //     </div>
+    // )
+    return React.createElement(
+        'div',
+        {id: "hello", className: 'dummyClass'},
+        React.createElement('h1', null, 'Hello Rahul')
     )
 }
 
