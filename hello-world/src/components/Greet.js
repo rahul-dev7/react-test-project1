d5c577fd2c77178dff7cae062b1d6ad7c486de3f
Rahul Prajapati <rahulp@ifourtechnolab.com>
2023-01-17 15:59:04 +0530

    Jsx

---
+++ hello-world/src/components/Greet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/* Simple Way  */
+
+// function Greet() {
+//     return <h1>Hello Rahul</h1>;
+// }
+
+/**** Arrow Way ****/ 
+const Greet = () => <h1>Hello Rahul</h1>;
+
+export default Greet;
