ea2dbfa51287a291d424c547b0e56dd924fc49d8
Rahul Prajapati <rahulp@ifourtechnolab.com>
2023-02-05 22:12:30 +0530

    conditional rendering.

--- hello-world/src/components/UserGreeting.js
+++ hello-world/src/components/UserGreeting.js
@@ -9,13 +9,44 @@ class UserGreeting extends Component {
       }
     }
     
+    
     render() {
-        if(this.state.isLoggingIn) {
-            return <div>Welcome Rahul</div>
-        }
-        else {
-            return <div>Welcome Guest</div>
-        }
+        // method 1 in Jsx 
+        
+        // if(this.state.isLoggingIn) {
+        //     return <div>Welcome Rahul</div>
+        // }
+        // else {
+        //     return <div>Welcome Guest</div>
+        // }
+        
+
+        // method 2 Varchar Approach
+        // let message
+        // if(this.state.isLoggingIn) {
+        //     message = <div>Welcome Rahul</div>
+        // }
+        // else {
+        //     message = <div>Welcome Guest</div>
+        // }
+
+        // return <div>{message}</div>
+
+
+        // method 3 conditional Rendering
+        // return this.state.isLoggingIn ?
+        //     ( 
+        //         <div>Welcome Rahul</div> 
+        //     ) : ( 
+        //         <div>Welcome Guest</div> 
+        //     )
+
+        // method 3 short cricuit approach
+
+        return this.state.isLoggingIn && <div>Welcome Rahul</div>
+
+        // use method 3 and method 4
+        
         // return (
         //     <div>
         //         <div> Welcome Rahul</div>
