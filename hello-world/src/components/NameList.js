82fb61068a3fd9ac7d83a6e30349058fc4a89c89
Rahul Prajapati <rahulp@ifourtechnolab.com>
2023-02-11 21:06:08 +0530

    index as key anti pattern.

--- hello-world/src/components/NameList.js
+++ hello-world/src/components/NameList.js
@@ -10,35 +10,41 @@ function NameList() {
     // )
     
     //2. method is best way.
-    const persons = [
-        {   
-            id: 1,
-            name: "Rahul",
-            age: 10,
-            skill: 'React'
-        },
-        {   
-            id: 2,
-            name: "Anup",
-            age: 40,
-            skill: "Angular"
-        },
-        {   
-            id: 3,
-            name: "Ashish",
-            age: 20,
-            skill: ".Net"
-        },
-        {   
-            id: 4,
-            name: "nitin",
-            age: 50,
-            skill: "Php"
-        },
-    ];
+    // const persons = [
+    //     {   
+    //         id: 1,
+    //         name: "Rahul",
+    //         age: 10,
+    //         skill: 'React'
+    //     },
+    //     {   
+    //         id: 2,
+    //         name: "Anup",
+    //         age: 40,
+    //         skill: "Angular"
+    //     },
+    //     {   
+    //         id: 3,
+    //         name: "Ashish",
+    //         age: 20,
+    //         skill: ".Net"
+    //     },
+    //     {   
+    //         id: 4,
+    //         name: "nitin",
+    //         age: 50,
+    //         skill: "Php"
+    //     },
+    // ];
 
-    const personsList = persons.map(person => <Person key={person.id} person={person}></Person>);
-    return <div>{personsList}</div>
+    // const personsList = persons.map(person => <Person key={person.id} person={person}></Person>);
+    // return <div>{personsList}</div>
+
+    //index as key anti pattern
+     const names = ['Brance', 'Clark', 'Dinna'];
+     const personsList = names.map((name,index) => <h2 key={index}>{index}{name}</h2>);
+     return <div>{personsList}</div>
+
 
 }
 
