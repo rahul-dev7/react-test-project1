6ccf7116d6cdd459de8401f76b23c36f3c443d82
Rahul Prajapati <rahulp@ifourtechnolab.com>
2023-02-18 09:38:27 +0530

    24 -Compoent Updating Lifecycle Methods

--- hello-world/src/components/LifecycleA.js
+++ hello-world/src/components/LifecycleA.js
@@ -20,11 +20,33 @@ class LifecycleA extends Component {
         console.log('LifecycleA componentDidMount');
     }
 
+    changeState = () => {
+        this.setState({
+            name: 'WindIdeas'
+        })
+    }
+
+    shouldComponentUpdate() {
+        console.log('LifecycleA shouldComponentUpdate');
+        return true;
+    }
+
+    getSnapshotBeforeUpdate() {
+        console.log('LifecycleA getSnapshotBeforeUpdate');
+        return null;
+    }
+
+    componentDidUpdate() {
+        console.log('LifecycleA componentDidUpdate');
+
+    }
+
     render() {
         console.log('LifecycleA render');
         return (
             <div>
                 <div>Lifecycle A</div>
+                <button onClick={this.changeState}>Change state</button>
                 <LifecycleB></LifecycleB>
             </div>
           
