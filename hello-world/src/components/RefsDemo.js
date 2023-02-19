08601cf95dc788fbb94460176d7f926fd663c2f4
Rahul Prajapati <rahulp@ifourtechnolab.com>
2023-02-19 09:33:43 +0530

    28-callback Ref

--- hello-world/src/components/RefsDemo.js
+++ hello-world/src/components/RefsDemo.js
@@ -4,11 +4,18 @@ class RefsDemo extends Component {
   constructor(props) {
     super(props)
     this.inputRef = React.createRef();
+    this.cdRef = null;
+    this.setCbRef = element => {
+      this.cdRef = element
+    }
   }
   
   componentDidMount() {
-    this.inputRef.current.focus();
-    console.log(this.inputRef);
+    // this.inputRef.current.focus();
+    // console.log(this.inputRef);
+    if(this.cdRef) {
+      this.cdRef.focus()
+    }
   }
 
   clickHandler =  () => {
@@ -19,6 +26,7 @@ class RefsDemo extends Component {
     return (
       <div>
         <input type="text" ref={this.inputRef}/>
+        <input type="text" ref={this.setCbRef}/>
         <button onClick={this.clickHandler}>Click</button>
       </div>
     )
