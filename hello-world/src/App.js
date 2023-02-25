de82bf6448e36a8ed88efc7a8319b33768c084b2
Rahul Prajapati <rahulp@ifourtechnolab.com>
2023-02-25 09:39:13 +0530

    31-portals.

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css';
 import FRParentInput from './components/FRParentInput';
 import ParentComp from './components/ParentComp';
+import PortalDemo from './components/PortalDemo';
 import RefsDemo from './components/RefsDemo';
 import PureComp from './PureComp';
 class App extends Component {
@@ -10,7 +11,8 @@ class App extends Component {
       <div className="App">
         {/* <FocusInput/> */}
        {/* <RefsDemo></RefsDemo> */}
-       <FRParentInput/>
+       {/* <FRParentInput/> */}
+       <PortalDemo></PortalDemo>
       </div>
     )
   }
